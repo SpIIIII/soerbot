c65cc77c942050d72e22c56bebecb4678c3dbbf2
SpIIIII <Shipin16@gmail.com>
2019-03-28 12:03:17 +0300

    add sample of lib command

---
+++ commands/librarian/lib.command.php
@@ -0,0 +1,34 @@
+<?php
+return function ($client) {
+    return (new class($client) extends \CharlotteDunois\Livia\Commands\Command
+    {
+        function __construct(\CharlotteDunois\Livia\LiviaClient $client)
+        {
+            parent::__construct($client, array(
+                'name' => 'lib', // Give command name
+                'aliases' => array(),
+                'group' => 'utils', // Group in ['command', 'util']
+                'description' => '...', // Fill the description
+                'guildOnly' => false,
+                'throttling' => array(
+                    'usages' => 5,
+                    'duration' => 10
+                ),
+                'guarded' => true,
+                'args' => array( // If you need some variables you should either fill this section or remove it
+                    array(
+                        'key' => 'replaceme',
+                        'label' => 'replaceme',
+                        'prompt' => 'replaceme',
+                        'type' => 'replaceme'
+                    )
+                )
+            ));
+        }
+
+        function run(\CharlotteDunois\Livia\CommandMessage $message, \ArrayObject $args, bool $fromPattern)
+        {
+            return  $message->say('...');
+        }
+    });
+};
