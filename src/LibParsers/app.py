37ec06884f0c54ab05c90d55574aa685c343ac1e
SpIIIII <Shipin16@gmail.com>
2019-04-04 18:08:35 +0300

    tpy to implement Dependency injection

--- src/LibParsers/app.py
+++ src/LibParsers/app.py
@@ -1,5 +1,14 @@
 from connexion.resolver import RestyResolver
 import connexion
+from flask_injector import FlaskInjector
+from injector import Binder
+from services.provider import ItemsProvider
+
+def configure(binder: Binder) ->Binder:
+    binder.bind(
+        ItemsPrivider,
+        ItemsProvider([{"Name":"test one"}])
+    )
 
 if __name__ =='__main__':
     app = connexion.App(__name__,port= 9090, specification_dir='swagger/')
